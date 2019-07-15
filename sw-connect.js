7233992f4dc6fb3eefdd412b4f059d76a27d9ba6
Git Desktop <31106210+blkdgit@users.noreply.github.com>
2019-07-15 10:32:51 +0200

    urk

--- sw-connect.js
+++
@@ -1,2 +0,0 @@
-
-"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("sw.js").then(function(a){console.log("ServiceWorker registration successful with scope: ",a.scope)},function(a){console.error("ServiceWorker registration failed: ",a)})});
